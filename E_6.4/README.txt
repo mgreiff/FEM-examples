2ebbcb7b64435fb6473d4c7bd3fbf0b082b257cb
Marcus <marcusgreiff.93@hotmail.com>
2015-12-03 23:19:23 +0100

    Added method for computing four element nodes from triangular ones in DataC6

---
+++ E_6.4/README.txt
@@ -0,0 +1,3 @@
+Added method for converting data from triangular to quadratic elements - 
+this has to be updated as to make a closed loop, and in a direction around
+the z-axis (for integration later on).
--- E_6.4/dataC6E4.m
+++ E_6.4/dataC6E4.m
@@ -6,11 +6,8 @@ v = 0.3;
 t = 0.1; % OBS! arbitrarily set
 D = (E/((1+v)*(1-2*v))) .* [1-v, v, 0;   v, 1-v, 0; 0, 0, (1-2*v)/2];
 
-nelm = length(edof(:,1));
-ndof = max(max(edof));
-
 % Plots the original geometry
-if 0
+if 1
     hold on;
     figure(1)
     for ii = 1:length(ex(:,1))
@@ -21,10 +18,46 @@ if 0
         end
     end
 end
+
+% Transforms geometry to four-node elements
 edofHat = [];
-coord = [];
-for ii  = (1:4)
-    coord = [coord, [ex(ii,:); ey(ii,:)]];
+exHat = [];
+eyHat = [];
+nelm = length(edof(:,1))/4;
+for n = 1:nelm
+    dofs = [];
+    for ii  = 4*(n - 1) + (1:4)
+        dofs = [dofs , edof(ii,2:7)];
+    end
+
+    validDofs= [];
+    for dof = unique(dofs)
+        if sum(dofs == dof) == 2
+            validDofs = [validDofs, dof];
+        end
+    end
+
+    validCoords = [];
+    for ii  = 4*(n - 1) + (1:4)
+        for jj = 1:3
+            if sum(edof(ii,2*jj) == validDofs) > 0
+                validCoords = [validCoords, [ex(ii,jj);ey(ii,jj)]];
+            end
+        end
+    end
+    validCoords = unique(validCoords','rows')';
+    edofHat = [edofHat;n, validDofs];
+    exHat = [exHat;validCoords(1,:)];
+    eyHat = [eyHat;validCoords(2,:)];
 end
-coordHat = unique(coord' , 'rows')';
-plot(coordHat(1,:),coordHat(2,:),'*')
+if 1
+    hold on;
+    figure(1)
+    for ii = 1:length(exHat(:,1))
+        ind = [1,2,3,4,1];
+        for jj = 1:4
+            plot([exHat(ii,ind(jj)) , exHat(ii, ind(jj + 1))],...
+                 [eyHat(ii,ind(jj)) , eyHat(ii, ind(jj + 1))], 'r--')
+        end
+    end
+end
