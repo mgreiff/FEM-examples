354a0796bc4e952f41edc84fbc75fafc45b17ac9
Marcus Greiff <marcusgreiff.93@hotmail.com>
2019-02-28 20:28:04 +0100

    Enhancement - Add all of the examples to the test script

--- test/runtests.m
+++ test/runtests.m
@@ -1,9 +1,10 @@
 %% List of all examples
-example_dirs = {
-    '1.3',...
-    '1.5'};
+example_dirs = {'1.3','1.5','2.5','2.6','6.1','6.2','6.3','6.4','8.1','9.1'};
 
 %% Attempt to run all of the examples
+fprintf('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+fprintf('~~~ Running all of the examples ~~~\n')
+fprintf('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 for ii = 1:length(example_dirs)
     fprintf(['Test of exercise ',example_dirs{ii}, ' : '])
     try
@@ -11,6 +12,5 @@ for ii = 1:length(example_dirs)
         fprintf('Pass\n')
     catch
         fprintf('Fail\n')
-    end
-    
+    end 
 end
